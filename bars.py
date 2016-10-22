323e0a5d9eae11babc70112216279f04d294fd39
hxndgtlmg <hxndgtlmg@users.noreply.github.com>
2016-10-22 15:14:47 +0300

    Написан алгоритм

--- bars.py
+++ bars.py
@@ -1,21 +1,48 @@
 import json
 
-
 def load_data(filepath):
-    pass
-
+    with open(filepath) as file:
+        data = json.loads(file.read())
+    return data
 
 def get_biggest_bar(data):
-    pass
-
+    biggest_bar = {"Cells":{"SeatsCount":float("-inf")}}
+    for bar in data:
+        if biggest_bar["Cells"]["SeatsCount"] < bar["Cells"]["SeatsCount"]:
+            biggest_bar = bar
+    return biggest_bar
 
 def get_smallest_bar(data):
-    pass
-
+    smallest_bar = {"Cells":{"SeatsCount":float("inf")}}
+    for bar in data:
+        if smallest_bar["Cells"]["SeatsCount"] > bar["Cells"]["SeatsCount"]:
+            smallest_bar = bar
+    return smallest_bar
 
 def get_closest_bar(data, longitude, latitude):
-    pass
+    _distance = float("inf")
+    longitude = float(longitude)
+    latitude = float(latitude)
+    closest_bar = {}
+    for bar in data:
+        bar_geo = bar["Cells"]["geoData"]["coordinates"]
+        bar_distance = distance([longitude, latitude], bar_geo)
+        if _distance > bar_distance:
+            _distance = bar_distance
+            closest_bar = bar
+    closest_bar["Cells"]["Distance"] = _distance
+    return closest_bar
 
+def distance(p1, p2):
+    return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
 
 if __name__ == '__main__':
-    pass
+    data = load_data(input("Введите путь к JSON файлу: "))
+    print()
+    biggest_bar = get_biggest_bar(data)
+    print("Самый большой бар: {0}\nКоличество мест: {1}\nАдрес: {2}\n".format(biggest_bar["Cells"]["Name"], biggest_bar["Cells"]["SeatsCount"], biggest_bar["Cells"]["Address"]))
+    smallest_bar = get_smallest_bar(data)
+    print("Самый маленький бар: {0}\nКоличество мест: {1}\nАдрес: {2}\n".format(smallest_bar["Cells"]["Name"], smallest_bar["Cells"]["SeatsCount"], smallest_bar["Cells"]["Address"]))
+    longitude, latitude = input("Введите долготу и широту через пробел: ").split()[:2]
+    closest_bar = get_closest_bar(data, longitude, latitude)
+    print("Самый близкий бар: {0}\nКоличество мест: {1}\nАдрес: {2}\nРасстояние: {3}\n".format(closest_bar["Cells"]["Name"], closest_bar["Cells"]["SeatsCount"], closest_bar["Cells"]["Address"], closest_bar["Cells"]["Distance"]))
