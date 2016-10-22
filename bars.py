7615ef2a9161913d741711243f8969210c724101
hxndgtlmg <hxndgtlmg@users.noreply.github.com>
2016-10-22 23:56:04 +0300

    Переписан под PEP8

--- bars.py
+++ bars.py
@@ -1,23 +1,27 @@
 import json
 
+
 def load_data(filepath):
     with open(filepath) as file:
         data = json.loads(file.read())
     return data
 
+
 def get_biggest_bar(data):
-    biggest_bar = {"Cells":{"SeatsCount":float("-inf")}}
+    biggest_bar = {"Cells": {"SeatsCount": float("-inf")}}
     for bar in data:
         if biggest_bar["Cells"]["SeatsCount"] < bar["Cells"]["SeatsCount"]:
             biggest_bar = bar
     return biggest_bar
 
+
 def get_smallest_bar(data):
-    smallest_bar = {"Cells":{"SeatsCount":float("inf")}}
+    smallest_bar = {"Cells": {"SeatsCount": float("inf")}}
     for bar in data:
         if smallest_bar["Cells"]["SeatsCount"] > bar["Cells"]["SeatsCount"]:
             smallest_bar = bar
     return smallest_bar
+
 
 def get_closest_bar(data, longitude, latitude):
     _distance = float("inf")
@@ -33,16 +37,36 @@ def get_closest_bar(data, longitude, latitude):
     closest_bar["Cells"]["Distance"] = _distance
     return closest_bar
 
+
 def distance(p1, p2):
     return ((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)**0.5
+
 
 if __name__ == '__main__':
     data = load_data(input("Введите путь к JSON файлу: "))
     print()
     biggest_bar = get_biggest_bar(data)
-    print("Самый большой бар: {0}\nКоличество мест: {1}\nАдрес: {2}\n".format(biggest_bar["Cells"]["Name"], biggest_bar["Cells"]["SeatsCount"], biggest_bar["Cells"]["Address"]))
+    print("Самый большой бар: {0}\n"
+          "Количество мест: {1}\n"
+          "Адрес: {2}\n".format(
+                            biggest_bar["Cells"]["Name"],
+                            biggest_bar["Cells"]["SeatsCount"],
+                            biggest_bar["Cells"]["Address"]))
     smallest_bar = get_smallest_bar(data)
-    print("Самый маленький бар: {0}\nКоличество мест: {1}\nАдрес: {2}\n".format(smallest_bar["Cells"]["Name"], smallest_bar["Cells"]["SeatsCount"], smallest_bar["Cells"]["Address"]))
-    longitude, latitude = input("Введите долготу и широту через пробел: ").split()[:2]
-    closest_bar = get_closest_bar(data, longitude, latitude)
-    print("Самый близкий бар: {0}\nКоличество мест: {1}\nАдрес: {2}\nРасстояние: {3}\n".format(closest_bar["Cells"]["Name"], closest_bar["Cells"]["SeatsCount"], closest_bar["Cells"]["Address"], closest_bar["Cells"]["Distance"]))
+    print("Самый маленький бар: {0}\n"
+          "Количество мест: {1}\n"
+          "Адрес: {2}\n".format(
+                            smallest_bar["Cells"]["Name"],
+                            smallest_bar["Cells"]["SeatsCount"],
+                            smallest_bar["Cells"]["Address"]))
+    lon, lat = input("Введите долготу и широту через пробел: ").split()[:2]
+    closest_bar = get_closest_bar(data, lon, lat)
+    print("Самый близкий бар: {0}\n"
+          "Количество мест: {1}\n"
+          "Адрес: {2}\n"
+          "Расстояние: {3}\n".format(
+                                closest_bar["Cells"]["Name"],
+                                closest_bar["Cells"]["SeatsCount"],
+                                closest_bar["Cells"]["Address"],
+                                closest_bar["Cells"]["Distance"]))
+
